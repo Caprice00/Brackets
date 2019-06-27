7af6adc5c19afdb7a6ead50faa9179daf5fadd77
Caprice00 <32291691+Caprice00@users.noreply.github.com>
2019-06-27 18:39:28 +0530

    spawner and Hexagon Scripts

---
+++ Brackets_o/Assets/spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float spawnRate = 1f;
+
+    public GameObject hexagonPrefab;
+
+    float nextTimeToSpawn = 0f;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextTimeToSpawn)
+        {
+            Instantiate(hexagonPrefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
