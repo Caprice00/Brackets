0afadab0d0dd1e6897ca061dd58e422573494f97
Caprice00 <32291691+Caprice00@users.noreply.github.com>
2019-06-27 17:50:01 +0530

    Player Script added
    Player Movement script added

---
+++ Brackets_o/Assets/player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 600;
+
+    float movement = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement =Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+    }
+}
